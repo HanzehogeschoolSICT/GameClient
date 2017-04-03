b141a649faf0a77d1cba7d7061b4e5e8711b619f
markdissel1992 <m.l.dissel@st.hanze.nl>
2017-04-03 15:50:43 +0200

    Uploaded correct files

---
+++ src/game/tictactoe/Model.java
@@ -0,0 +1,25 @@
+package game.tictactoe;
+
+/**
+ *
+ * @author Mark
+ */
+public class Model {
+    char[][] board;
+
+    public Model() {
+    board = new char[3][3];
+    }
+
+    public char getSymbol(int row, int column) {
+        return board[row][column];
+    }
+
+    public void setSymbol(int row, int column, char symbol) {
+        board[row][column] = symbol;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+}
