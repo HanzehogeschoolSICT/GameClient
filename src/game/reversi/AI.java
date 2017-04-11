a5c073b60c9afb008ec28142955d7e21c0c67fa2
NiseVoid <nisevoid@gmail.com>
2017-04-11 12:46:01 +0200

    Imporve reversi AI

--- src/game/reversi/AI.java
+++ src/game/reversi/AI.java
@@ -18,33 +18,33 @@ public class AI {
         }
     }
 
-    Point nextMove() {
-        List<Pair<Point, Integer>> options = getOptions(model);
+    public Point nextMove() {
+        List<Pair<Point, Integer>> options = getOptions(model, whoami, enemy);
 
         Point bestPoint = null;
         double bestChance = -1;
 
-        Model n = new Model(model);
-
         for (Pair<Point, Integer> pair : options) {
             Point p = pair.getKey();
-            double chance = (double)pair.getValue();
-            if ((p.x == 0 || p.x == 7) &&
-                (p.y == 0 || p.y == 7)) {
-                chance += 4.5;
-            }
+            double chance = (double)pair.getValue() + getBias(p);
 
-            if ((p.x == 1 || p.x == 6) &&
-                (p.y == 1 || p.y == 6)) {
-                chance -= 3.1;
-            }
+			Model n = new Model(model);
+			n.setSymbol(p.x, p.y, whoami);
 
-            if (((p.x == 1 || p.x == 6) && (p.y == 0 || p.y == 7)) ||
-                ((p.y == 1 || p.y == 6) && (p.x == 0 || p.x == 7))) {
-                chance -= 2.05;
-            }
+			// Calculate best enemy move
+			Pair<Point, Double> ep = getBestMove(n, p, enemy, whoami);
+			if (ep.getKey() != null) {
+				chance -= (ep.getValue() * 0.9);
 
-            n.setSymbol(p.x, p.y, (char)(chance + '0'));
+				n.setSymbol(ep.getKey().x, ep.getKey().y, enemy);
+
+				// Calculate best next move
+				Pair<Point, Double> np = getBestMove(n, p, whoami, enemy);
+				if (ep.getKey() != null) {
+					chance += (np.getValue() * 0.6);
+				}
+			}
+
             if (chance > bestChance || bestPoint == null) {
                 bestPoint = p;
                 bestChance = chance;
@@ -54,13 +54,32 @@ public class AI {
         return bestPoint;
     }
 
+    private double getBias(Point p) {
+        if ((p.x == 0 || p.x == 7) &&
+            (p.y == 0 || p.y == 7)) {
+            return 4;
+        }
+
+        if ((p.x == 1 || p.x == 6) &&
+            (p.y == 1 || p.y == 6)) {
+            return -3;
+        }
+
+        if (((p.x == 1 || p.x == 6) && (p.y == 0 || p.y == 7)) ||
+            ((p.y == 1 || p.y == 6) && (p.x == 0 || p.x == 7))) {
+            return -1.9;
+        }
+
+        return 0;
+    }
+
     private Point[] dirs = new Point[]{
         new Point(-1, -1), new Point( 0, -1), new Point( 1, -1),
         new Point(-1,  0),                    new Point( 1,  0),
         new Point(-1,  1), new Point( 0,  1), new Point( 1,  1),
     };
 
-    private List<Pair<Point, Integer>> getOptions(Model m) {
+    private List<Pair<Point, Integer>> getOptions(Model m, char me, char op) {
         List<Pair<Point, Integer>> options = new ArrayList<Pair<Point, Integer>>();
 
         char[][] board = m.getBoard();
@@ -82,11 +101,11 @@ public class AI {
                             break;
                         }
 
-                        if (board[n.x][n.y] == enemy) {
+                        if (board[n.x][n.y] == op) {
                             en++;
                             continue;
                         }
-                        if (board[n.x][n.y] == whoami && en > 0) {
+                        if (board[n.x][n.y] == me && en > 0) {
                             points += en;
                         }
                         break;
@@ -97,5 +116,22 @@ public class AI {
         }
 
         return options;
+    }
+
+    private Pair<Point, Double> getBestMove(Model m, Point p, char me, char op) {
+        List<Pair<Point,Integer>> enemyOptions = getOptions(m, me, op);
+
+        Double bestChance = 0.0;
+		Point bestPoint = null;
+
+        for (Pair<Point, Integer> ep : enemyOptions) {
+            double chance = (double)ep.getValue() + (0.70*getBias(ep.getKey()));
+            if (chance > bestChance || bestPoint == null) {
+				bestChance = chance;
+				bestPoint = ep.getKey();
+			}
+        }
+
+        return new Pair<Point, Double>(bestPoint, bestChance);
     }
 }
